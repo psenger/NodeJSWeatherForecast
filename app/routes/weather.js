5f009048758e2f5a9662050161cda5ea1b22967e
psenger <philip.a.senger@cngrgroup.com>
2015-03-23 21:54:54 +1100

    fixed the routing problem

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -2,6 +2,10 @@
     "use strict";
     module.exports = function (server) {
         var forecastclient = require('../data/forecastclient');
+        /**
+         * I could have done something fancy with regular expressions, but decided to keep it simple.
+         */
+        server.get('/weather/:location/', forecastclient.fetch);
         server.get('/weather/:location/:weekday', forecastclient.fetch);
     };
 }());
