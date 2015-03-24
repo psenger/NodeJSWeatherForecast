180d1b8a3e4080909b6c0798f62fbabfe0a838c6
psenger <philip.a.senger@cngrgroup.com>
2015-03-24 21:54:45 +1100

    setting the port was inverted and the env value should have had precedence over the conf version

--- bin/launch.js
+++ bin/launch.js
@@ -4,7 +4,7 @@ var app = require('../app/index');
 var conf = require('../app/util/conf');
 
 // This will launch the server.
-var server = app.listen(conf.get("port") || process.env.PORT, function () {
+var server = app.listen(process.env.PORT || conf.get("port"), function () {
     console.log('%s listening at %s on port %s', app.name, app.url, app.address().port);
 });
 
