5f62b1ae4dee5d0e581b4b28e9fc69d5c5a80fe7
psenger <philip.a.senger@cngrgroup.com>
2015-03-24 19:44:08 +1100

    added debub

--- bin/launch.js
+++ bin/launch.js
@@ -3,6 +3,7 @@
 var app = require('../app/index');
 var conf = require('../app/util/conf');
 
+console.log( conf.get("port") );
 // This will launch the server.
 var server = app.listen(conf.get("port") || 8080, function () {
     console.log('%s listening at %s on port %s', app.name, app.url, app.address().port);
